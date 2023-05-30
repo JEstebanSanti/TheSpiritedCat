1d6a13bc35fb5016c29aed20bf45bed0b80f05a8
JEstebanSanti <a20311083@uthermosillo.edu.mx>
2023-05-30 02:11:11 -0700

    Añadido Todo LA UI del POS

--- PanelPrincipal.Designer.cs
+++ PanelPrincipal.Designer.cs
@@ -28,19 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            labelTitle = new Label();
+            labelDeveloper = new Label();
+            labelDate = new Label();
+            dataGridViewProductos = new DataGridView();
+            textBoxCodigo = new TextBox();
+            buttonIce = new Button();
+            buttonBeefService = new Button();
+            pictureBoxIcon = new PictureBox();
+            labelTotal = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProductos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxIcon).BeginInit();
             SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            labelTitle.AutoSize = true;
+            labelTitle.Location = new Point(346, 32);
+            labelTitle.Name = "labelTitle";
+            labelTitle.Size = new Size(37, 15);
+            labelTitle.TabIndex = 0;
+            labelTitle.Text = "Titulo";
+            // 
+            // labelDeveloper
+            // 
+            labelDeveloper.AutoSize = true;
+            labelDeveloper.Location = new Point(316, 59);
+            labelDeveloper.Name = "labelDeveloper";
+            labelDeveloper.Size = new Size(94, 15);
+            labelDeveloper.TabIndex = 1;
+            labelDeveloper.Text = "Desarrollado por";
+            // 
+            // labelDate
+            // 
+            labelDate.AutoSize = true;
+            labelDate.Location = new Point(345, 74);
+            labelDate.Name = "labelDate";
+            labelDate.Size = new Size(38, 15);
+            labelDate.TabIndex = 2;
+            labelDate.Text = "label3";
+            // 
+            // dataGridViewProductos
+            // 
+            dataGridViewProductos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewProductos.GridColor = SystemColors.ActiveBorder;
+            dataGridViewProductos.Location = new Point(234, 117);
+            dataGridViewProductos.Name = "dataGridViewProductos";
+            dataGridViewProductos.ReadOnly = true;
+            dataGridViewProductos.RowTemplate.Height = 25;
+            dataGridViewProductos.Size = new Size(240, 150);
+            dataGridViewProductos.TabIndex = 3;
+            // 
+            // textBoxCodigo
+            // 
+            textBoxCodigo.Location = new Point(518, 379);
+            textBoxCodigo.Name = "textBoxCodigo";
+            textBoxCodigo.Size = new Size(100, 23);
+            textBoxCodigo.TabIndex = 4;
+            // 
+            // buttonIce
+            // 
+            buttonIce.Location = new Point(90, 418);
+            buttonIce.Name = "buttonIce";
+            buttonIce.Size = new Size(75, 23);
+            buttonIce.TabIndex = 5;
+            buttonIce.Text = "Bebidas";
+            buttonIce.UseVisualStyleBackColor = true;
+            // 
+            // buttonBeefService
+            // 
+            buttonBeefService.Location = new Point(217, 418);
+            buttonBeefService.Name = "buttonBeefService";
+            buttonBeefService.Size = new Size(130, 23);
+            buttonBeefService.TabIndex = 6;
+            buttonBeefService.Text = "Servicio de Carne Asada";
+            buttonBeefService.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxIcon
+            // 
+            pictureBoxIcon.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBoxIcon.Location = new Point(46, 74);
+            pictureBoxIcon.Name = "pictureBoxIcon";
+            pictureBoxIcon.Size = new Size(100, 50);
+            pictureBoxIcon.TabIndex = 7;
+            pictureBoxIcon.TabStop = false;
+            // 
+            // labelTotal
+            // 
+            labelTotal.AutoSize = true;
+            labelTotal.Location = new Point(580, 109);
+            labelTotal.Name = "labelTotal";
+            labelTotal.Size = new Size(32, 15);
+            labelTotal.TabIndex = 8;
+            labelTotal.Text = "Total";
             // 
             // PanelPrincipal
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(847, 469);
+            Controls.Add(labelTotal);
+            Controls.Add(pictureBoxIcon);
+            Controls.Add(buttonBeefService);
+            Controls.Add(buttonIce);
+            Controls.Add(textBoxCodigo);
+            Controls.Add(dataGridViewProductos);
+            Controls.Add(labelDate);
+            Controls.Add(labelDeveloper);
+            Controls.Add(labelTitle);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "PanelPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "TheSpiritedCat";
+            WindowState = FormWindowState.Maximized;
             Load += PanelPrincipal_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProductos).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxIcon).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label labelTitle;
+        private Label labelDeveloper;
+        private Label labelDate;
+        private DataGridView dataGridViewProductos;
+        private TextBox textBoxCodigo;
+        private Button buttonIce;
+        private Button buttonBeefService;
+        private PictureBox pictureBoxIcon;
+        private Label labelTotal;
     }
 }
--- PanelPrincipal.cs
+++ PanelPrincipal.cs
@@ -1,8 +1,21 @@
+using MySql.Data.MySqlClient;
+using System.Reflection.Metadata.Ecma335;
+
 namespace TheSpiritedCat
 {
     public partial class PanelPrincipal : Form
-    { 
+    {
+        // Class Variables
 
+        // Mysql
+        MySqlConnection mysqlCon = new MySqlConnection("server=localhost;database=pos;uid=root;pwd=123");
+        MySqlCommand cmd;
+        MySqlDataReader reader;
+
+        // Mysql Strings
+        string query = "";
+        string quantity = "";
+        double total = 0;
 
         public PanelPrincipal()
         {
@@ -11,6 +24,62 @@ namespace TheSpiritedCat
 
         private void PanelPrincipal_Load(object sender, EventArgs e)
         {
+            // Fonts
+            Font fontTitle = new Font("Arial", 29, FontStyle.Bold);
+            Font font = new Font("Arial", 18);
+            Font fontDev = new Font("Arial", 18, FontStyle.Italic);
+            labelTitle.Font = fontTitle;
+            labelTotal.Font = fontTitle;
+            labelDate.Font = font;
+            labelDeveloper.Font = font;
+            textBoxCodigo.Font = font;
+
+            // Data Propieties
+            pictureBoxIcon.Image = Image.FromFile("../../../img/the-spirited-cat-logo.png");
+            pictureBoxIcon.SizeMode = PictureBoxSizeMode.Zoom;
+            labelDeveloper.Text = "Developed By JBanS";
+            labelTitle.Text = "The Spirited Cat";
+            pictureBoxIcon.Size = new Size(300, labelDate.Height + labelDeveloper.Height + labelTitle.Height + 90);
+            this.BackColor = Color.FromArgb(242, 242, 242);
+            buttonBeefService.FlatStyle = FlatStyle.Flat;
+            buttonBeefService.Image = Image.FromFile("../../../img/filete-servicio-100.png");
+            buttonBeefService.ImageAlign = ContentAlignment.MiddleCenter;
+            buttonIce.ImageAlign = ContentAlignment.MiddleCenter;
+            buttonIce.Font = new Font("Arial", 14, FontStyle.Bold);
+            buttonBeefService.Font = new Font("Arial", 14, FontStyle.Bold);
+            buttonBeefService.Size = new Size(180, 180);
+            buttonIce.Size = new Size(180, 180);
+            buttonBeefService.Text = "Servicio Carne";
+            buttonIce.FlatStyle = FlatStyle.Flat;
+            buttonIce.Text = "Bebidas ICE";
+            buttonBeefService.TextAlign = ContentAlignment.BottomCenter;
+            buttonIce.TextAlign = ContentAlignment.BottomCenter;
+            buttonIce.Image = Image.FromFile("../../../img/soda-servicio-100.png");
+
+
+
+            // date label
+            labelDate.Text = DateTime.Now.ToLongTimeString() + " " + DateTime.Now.ToLongDateString();
+            // Items Location
+            pictureBoxIcon.Location = new Point(10, 10);
+            labelTitle.Location = new Point((this.Width / 2) - (labelTitle.Width / 2), 10);
+            labelDeveloper.Location = new Point(((this.Width / 2) - (labelDeveloper.Width / 2)), labelTitle.Height + labelDeveloper.Height);
+            labelDate.Location = new Point((this.Width / 2) - (labelDate.Width / 2), (labelTitle.Height + labelDeveloper.Height + labelDate.Height));
+            dataGridViewProductos.Location = new Point((10), (labelTitle.Height + labelDeveloper.Height + labelDate.Height + pictureBoxIcon.Height));
+            dataGridViewProductos.Width = (this.Width - 20);
+            dataGridViewProductos.Height = (this.Height / 4) * 2;
+            dataGridViewProductos.BackgroundColor = Color.White;
+            textBoxCodigo.Location = new Point(10, labelTitle.Height+pictureBoxIcon.Height);
+            textBoxCodigo.Width = this.Width - 20;
+            buttonBeefService.Location = new Point(this.Width/2-buttonBeefService.Width/2, dataGridViewProductos.Height + labelDeveloper.Height + labelTitle.Height + labelDate.Height + pictureBoxIcon.Height);
+            buttonIce.Location = new Point(this.Width / 2 - buttonIce.Width / 2 - buttonIce.Width, dataGridViewProductos.Height + labelDeveloper.Height + labelTitle.Height + labelDate.Height + pictureBoxIcon.Height);
+            labelTotal.Location = new Point((this.Width / 2) + pictureBoxIcon.Width + labelTitle.Width, labelTitle.Height + labelDeveloper.Height);
+
+
+
+
+
+
 
         }
     }
